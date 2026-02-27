b0b3678d45a218e050da9643d3c772d0aa873ca6
Yar14k <tabachuk1406@gmail.com>
2026-02-27 20:21:14 +0100

    Add solution

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -6,11 +6,9 @@ type Props = {
   onAdd: (movie: Movie) => void;
 };
 
-const pattern = new RegExp(
-  '^((([A-Za-z]{3,9}:(?://)?)(?:[-;:&=+$,\\w]+@)?[A-Za-z0-9.-]+|' +
-    '(?:www\\.|[-;:&=+$,\\w]+@)[A-Za-z0-9.-]+)((?:/[+~%/\\.\\w-_]*)?\\??)?' +
-    '(?:[-+=&;%@,.\\w_]*)#?(?:[,.!/\\\\\\w]*))?)$',
-);
+const pattern =
+// eslint-disable-next-line max-len
+  /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
 
 const validateUrl = (value: string) => pattern.test(value.trim());
 
